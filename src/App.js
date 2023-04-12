0153eb3697b94bd1fec19d963c2b990b13a58993
“Abdul <kaazees31@gmail.com>
2023-04-12 14:07:32 +0530

    upd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./sb-admin-2.min.css";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import Chart from "./Bar";
+import Footer from "./components/Footer";
+import LogoutModel from "./components/LogoutModel";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Navbar />
+            <div class="container-fluid">
+              <Dashboard />
+              <div class="row">
+                <Chart />
+                <Footer />
+                <div className="row">
+                  <LogoutModel />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
